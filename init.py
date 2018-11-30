38593414ef1be5fbc648a2053631cdf4e41d9224
PurpleStripedUnicorn <dirck.code@gmail.com>
2018-11-30 18:07:29 +0100

    Cleaned up code

--- init.py
+++ init.py
@@ -9,18 +9,30 @@ from random import *
 
 
 
+# parse a formaula, which includes 't' as parameter
+# parameter is given, value on this t is output
 def parse_formula (formula, parameter):
 
+    # compile formula and set parameter 't'
     code = parser.expr(formula).compile()
     t = parameter
+    # try to evaluate code
+    # this is in case of wrong user input or invalid values
     try:
+        # evaulate code and get value
         ev = eval(code)
     except:
+        # return 0 on failure
+        # BUG: currently projects straight line on x-axis
         return 0
+    # return the value
     return ev
 
+# draw a given formaula (included in data) on a grid
+# return grid with formula draw on it
 def draw_formula (grid, data):
 
+    # convert given line color to proper object type
     line_color = (data["colors"]["line"][0],
         data["colors"]["line"][1],
         data["colors"]["line"][2])
@@ -33,52 +45,69 @@ def draw_formula (grid, data):
         x = parse_formula(data["graph"]["x"], t)
         y = parse_formula(data["graph"]["y"], t)
 
-        x_min = data["window"]["x"]["min"]
-        y_min = data["window"]["y"]["min"]
-        x_max = data["window"]["x"]["max"]
-        y_max = data["window"]["y"]["max"]
+        # convert coords to point coords on the grid
 
-        x_valid = x < x_max and x > x_min
-        y_valid = y < y_max and y > y_min
+        # convert x-coord, round to nearest whole integer point
+        gx = x
+        gx -= data["window"]["x"]["min"]
+        gx /= (data["window"]["x"]["max"] - data["window"]["x"]["min"])
+        gx *= data["window"]["resolution"]["x"]
+        gx = int(round(gx, 0))
 
-        if x_valid and y_valid:
+        # convert y-coord, round to nearest whole integer point
+        gy = y
+        gy -= data["window"]["y"]["min"]
+        gy /= (data["window"]["y"]["max"] - data["window"]["y"]["min"])
+        gy *= data["window"]["resolution"]["y"]
+        gy = data["window"]["resolution"]["y"] - gy
+        gy = int(round(gy, 0))
 
-            # convert coords to point coords on the grid
-            gx = x
-            gx -= data["window"]["x"]["min"]
-            gx /= (data["window"]["x"]["max"] - data["window"]["x"]["min"])
-            gx *= data["window"]["resolution"]["x"]
-            gx = int(round(gx, 0))
+        # get the maximum values of x and y as grid point coords
+        max_x = len(grid[0]) - 1
+        max_y = len(grid) - 1
 
-            gy = y
-            gy -= data["window"]["y"]["min"]
-            gy /= (data["window"]["y"]["max"] - data["window"]["y"]["min"])
-            gy *= data["window"]["resolution"]["y"]
-            gy = data["window"]["resolution"]["y"] - gy
-            gy = int(round(gy, 0))
+        # project the point on the grid
+        # first, get the dotsize, projected as square on the image
+        dotsize = data["dotsize"]
+        # go through every point that needs to be painted on the grid
+        # first, go through x values
+        for i in range(gx - dotsize, gx + dotsize + 1):
+            # go through y values
+            for j in range(gy - dotsize, gy + dotsize + 1):
+                # check if point is within range of allowed values
+                if j >= 0 and j <= max_y and i >= 0 and i <= max_x:
+                    # paint pixel on image
+                    grid[j][i] = line_color
 
-            # project the point on the grid
-            dotsize = data["dotsize"]
-            for i in range(gx - dotsize, gx + dotsize + 1):
-                for j in range(gy - dotsize, gy + dotsize + 1):
-                    if j >= 0 and j < len(grid) and i >= 0 and i < len(grid[0]):
-                        grid[j][i] = line_color
-
+        # go check next value of t
         t += data["window"]["t"]["step"]
 
+    # return grid as output
     return grid
 
+# function to add an image (defined as data), to input grid
+# input are:
+# - grid to change
+# - filename of file of image data
+# - name of image data container in JSON file
+# - coordinates (x, y) of upper left corner
+# - (optional) scaling of image
 def add_image (grid, filename, name, color, x, y, scale = 1):
 
+    # get max allowed coords on the image
     x_max = len(grid[0]) - 1
     y_max = len(grid) - 1
 
     # get image data as array
     ndata = getjson("imagedata/" + filename + ".json")[name]
 
+    # go thwough every data point in the image data
     for i in range(0, len(ndata[0])):
         for j in range(0, len(ndata)):
+            # check if pixel is marked
             if ndata[j][i] == 1:
+                # go through every pixel in data point
+                # only more than 1 with scaling set
                 for ii in range(0, scale):
                     for jj in range(0, scale):
 
@@ -87,28 +116,39 @@ def add_image (grid, filename, name, color, x, y, scale = 1):
                         yc = j * scale + y + jj
 
                         if xc <= x_max and xc >= 0 and yc <= y_max and yc >= 0:
+                            # paint pixel on grid
                             grid[yc][xc] = color
 
+    # return generated grid
     return grid
 
+# add text (as image) to grid
+# text data is in JSON file
 def add_text (grid, text, x, y):
 
+    # calculate appropriate scaling for the text to insert
     scale = int(ceil(max([len(grid), len(grid[0])]) * 0.005))
 
+    # go through every character in the text
+    # keep track of the distance from the left and top of the start
     n = 0
     m = 0
     for t in text:
+        # check if line break is inserted, to add vertical spacing
         if t != "\n":
+            # calculate distance from top and left
             xspacing = x + 4 * n * scale
             yspacing = y + 6 * m * scale
+            # set color of text
             color = (0, 0, 0)
+            # add image data of text to the grid
             grid = add_image(grid, "text", t, color, xspacing, yspacing, scale)
             n += 1
         else:
             n = 0
             m += 1
 
-
+    # return generated new grid
     return grid
 
 
@@ -197,9 +237,12 @@ if mode == "parametric":
     # draw graph
     grid = draw_formula(grid, data)
 
+    # check if the formula should be shown on the output image
     if data["displayformula"]:
+        # prepare text to display
         grdes = "x(t) = " + data["graph"]["x"].lower()
         grdes += "\ny(t) = " + data["graph"]["y"].lower()
+        # add text to image
         grid = add_text(grid, grdes, 7, 4)
 
     # show image
